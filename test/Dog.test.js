4f29aab8d239c37f5bdd25fb24138a14a874887f
cumauu73 <cumauu73@icloud.com>
2023-08-23 08:32:02 -0500

    animal and dog test passed

--- test/Dog.test.js
+++ test/Dog.test.js
@@ -1,4 +1,32 @@
+const { Dog } = require('../lib')
+
 describe('Dog Class', () => {
+     it('our animal get correct age', () => {
+        //arrange
+        const name = 'mabel'
+        const sound = 'bark bark'
+        const age = 10
+        const breed = 'pitbull'
+        //Act
+        const dog = new Dog(name, sound, age, breed)
+        //Assert
+        expect(dog.age).toBe(age)
+    })
+    it('our animal get correct breed', () => {
+         //arrange
+         const name = 'mabel'
+         const sound = 'woof woof'
+         const age = 10
+         const breed = 'pitbull'
+         //Act
+         const dog = new Dog(name, sound, age, breed)
+         //Assert
+         expect(dog.breed).toBe(breed)
+    })
+
+    it('', () => {
+
+    })
 
 
     
