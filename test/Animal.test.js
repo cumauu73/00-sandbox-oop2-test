03398d2a423beff7b9bb694f1461bec3fa9b3b70
cumauu73 <cumauu73@icloud.com>
2023-08-23 07:26:47 -0500

    animal test passed

--- test/Animal.test.js
+++ test/Animal.test.js
@@ -3,14 +3,42 @@ const { Animal } = require('../lib')
 //test suite
 describe('Animal Class', () => {
     it('our animal get correct age', () => {
+        //arrange
+        const age = 10
+        const breed = 'tabby'
         //Act
-        //Arrange
+        const animal = new Animal(age, breed)
         //Assert
+        expect(animal.age).toBe(age)
     })
     it('our animal get correct breed', () => {
-
+         //arrange
+         const age = 10
+         const breed = 'tabby'
+         //Act
+         const animal = new Animal(age, breed)
+         //Assert
+         expect(animal.breed).toBe(breed)
     })
-    it('our animal get correct isalive ', () => {
+    it('decrament hungerlevel by timepass 1', () => {
+         //arrange
+         const age = 10
+         const breed = 'tabby'
+         //Act
+         const animal = new Animal(age, breed)
+         animal.timePasses()
+         //assert
+         expect(animal.hungerLevel).toBe(99)
+    })
 
+    it('increase hungerlevel by timepass 1', () => {
+         //arrange
+         const age = 10
+         const breed = 'tabby'
+         //Act
+         const animal = new Animal(age, breed)
+         animal.eat()
+         //assert
+         expect(animal.hungerLevel).toBe(101)
     })
 })
