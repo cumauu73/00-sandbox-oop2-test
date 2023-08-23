9711db8453399162993682f2ea731623f2e75515
cumauu73 <cumauu73@icloud.com>
2023-08-23 09:02:30 -0500

    all test passed

--- test/Cat.test.js
+++ test/Cat.test.js
@@ -1,4 +1,56 @@
+const { Cat } = require('../lib')
+
 describe('Cat Class', () => {
+     it('our animal get correct breed', () => {
+        //arrange
+        const name = 'mabel'
+        const sound = 'bark bark'
+        const age = 10
+        const breed = 'tabby'
+        //Act
+        const cat = new Cat(name, sound, age, breed)
+        //Assert
+        expect(cat.age).toBe(age)
+    })
+    it('our animal get correct name', () => {
+         //arrange
+         const name = 'mabel'
+         const sound = 'meovvv'
+         const age = 10
+         const breed = 'tabby'
+         //Act
+         const cat = new Cat(name, sound, age, breed)
+         //Assert
+         expect(cat.breed).toBe(breed)
+    })
+
+    it('make sound with', () => {
+         //arrange
+         const name = 'mabel'
+         const sound = 'meovvv'
+         const age = 10
+         const breed = 'tabby'
+         //Act
+         const cat = new Cat(name, sound, age, breed)
+         const soundResult = cat.makeSound()
+         //assert
+         expect(cat.sound).toBe(sound)
+         expect(soundResult).toBe(sound)
+    })
+
+    it('correctly decement numlives', () => {
+        //arrange
+        const name = 'mabel'
+        const sound = 'meovvv'
+        const age = 10
+        const breed = 'tabby'
+        //Act
+        const cat = new Cat(name, sound, age, breed)
+        cat.loseLife()
+        const remainingLives = cat.getNumLives()
+        //assert
+        expect(remainingLives).toBe(8)
+   })
 
 
     
