8429ae5367fce07d5995e5437f012a4a708c5b24
cumauu73 <cumauu73@icloud.com>
2023-08-23 06:57:41 -0500

    test test

--- test/Animal.test.js
+++ test/Animal.test.js
@@ -1,19 +1,16 @@
-//age
-//breed
-//isAlive
+const { Animal } = require('../lib')
 
-
+//test suite
 describe('Animal Class', () => {
     it('our animal get correct age', () => {
-
+        //Act
+        //Arrange
+        //Assert
     })
     it('our animal get correct breed', () => {
 
     })
-    it('our animal get correct isalive boolean', () => {
+    it('our animal get correct isalive ', () => {
 
     })
-
-
-
 })
---
+++ test/Dog.test.js
@@ -0,0 +1,5 @@
+describe('Dog Class', () => {
+
+
+    
+})
