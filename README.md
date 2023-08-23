27ee7f3967be674ec8b46be5fb9a3cec917384c4
cumauu73 <cumauu73@icloud.com>
2023-08-23 06:06:08 -0500

    pet js dogjs cat js created

--- README.md
+++
@@ -1,1 +0,0 @@
-# 00-sandbox-oop2-test
---
+++ index.js
@@ -0,0 +1,15 @@
+const Dog = require('./lib/Dog')
+const Cat = require('./lib/Cat')
+
+
+
+
+const mabel = new Dog('mabel', 1, 'old english dog')
+const alpha = new Dog('alpha', 3, 'teriior pitbull')
+const bruno = new Dog('bruno', 1.5, 'terrior pitbull')
+const ivy = new Cat('ivy', 9, 'long haired tabby')
+
+console.log(mabel.getNumLives())
+console.log(alpha.getNumLives())
+console.log(bruno.getNumLives())
+console.log(ivy.getNumLives())
